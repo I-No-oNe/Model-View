1b66a7b1830a9a34b93eb99ff90a8dd2650825e3
I-No-oNe <145749961+I-No-oNe@users.noreply.github.com>
2024-06-27 20:27:55 +0300

    Version 8.0
    *Improved the mod config system
    
    *Fixed a bug with off hand moving whenever moving the main hand slider
    
    *Improved GUI

--- src/main/java/net/i_no_am/viewmodel/utils/Utils.java
+++ src/main/java/net/i_no_am/viewmodel/utils/Utils.java
@@ -1,5 +1,6 @@
 package net.i_no_am.viewmodel.utils;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.i_no_am.viewmodel.client.Global;
 import net.i_no_am.viewmodel.client.ModVersionChecker;
 import net.minecraft.client.MinecraftClient;
@@ -19,5 +20,15 @@ public class Utils implements Global {
 
     public static void setChecked(boolean bool) {
         ModVersionChecker.checked = bool;
+    }
+    public static String getModVersion() {
+        String fullVersionString = FabricLoader.getInstance().getModContainer(modId).get().getMetadata().getVersion().getFriendlyString();
+        String[] parts = fullVersionString.split("-");
+        for (String part : parts) {
+            if (part.matches("\\d+\\.\\d+")) {
+                return part;
+            }
+        }
+        return "Unknown";
     }
 }
